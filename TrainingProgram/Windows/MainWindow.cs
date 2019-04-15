7bca6a8414340e93604180fc65d8b87eee5be64c
Артём <nemliy00@mail.ru>
2019-04-15 20:47:45 +0300

    n

--- TrainingProgram/Windows/MainWindow.cs
+++ TrainingProgram/Windows/MainWindow.cs
@@ -9,7 +9,6 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Windows;
 using System.IO;
-using Microsoft.DirectX.AudioVideoPlayback;
 using AxWMPLib;
 using System.Threading;
 using TrainingProgram.Class;
